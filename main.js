4766600410fdfc877ef20e5ea29aef6d13a32ab6
juan barchetta <barchettajuan.barche@gmail.com>
2025-07-23 15:34:53 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json()) // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })) // Parse URL-encoded bodies
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/ping', (req, res) => {
+    res.json({ message: 'pong' })
+})
+
+// Example POST endpoint
+app.post('/example', (req, res) => {
+  const { data } = req.body
+  res.json({ message: 'This is a POST request example', receivedData: data })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
