ec8ac65233ea01037455c6647f727e081ef5cc67
Mailen <mailengcopponi@gmail.com>
2025-07-24 14:37:58 -0300

    primer commit mai

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ app.use(express.json()) // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })) // Parse URL-encoded bodies
 
 app.get('/', (req, res) => {
-  res.send('1234567890')
+  res.send('Hola muchachoooo')
   //modification
 })
 
